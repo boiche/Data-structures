99a7fb2c8856f234dfe498ed8f8c7ef666ced5e1
Boil Marinov <boil_marinov@outlook.com>
2025-03-13 17:46:14 +0200

    Prefix sum

---
+++ Algorithms/Other/CollectionExtensions/PrefixSum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Algorithms.Other.CollectionExtensions
+{
+    public static partial class CollectionExtensions
+    {
+        public static IList<T> ToPrefixSum<T>(this IList<T> source) where T : IAdditionOperators<T, T, T>
+        {
+            List<T> result = new List<T>();
+            T sum = default;
+
+            for (int i = 0; i < source.Count; i++)
+            {
+                sum += source[i];
+                result.Add(sum);
+            }
+
+            return result;
+        }
+    }
+}
