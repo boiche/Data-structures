7092317e2ef1cda54abeeb4368293c659f11e3a9
Boil <boil.marinov16@gmail.com>
2025-03-13 13:35:17 +0200

    Range updater for IList<T> (#2)
    Co-authored-by: Boil Marinov <boil_marinov@outlook.com>

---
+++ Algorithms.Tests/Other/CollectionExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Algorithms.Other.CollectionExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using static Algorithms.Other.CollectionExtensions.CollectionExtensions;
+
+namespace Algorithms.Tests.Other
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        [TestMethod]
+        public void UpdateRange_Addition_WorksCorrectly()
+        {
+            int[] source = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            int[] expected = [6, 7, 8, 9, 10, 11, 12, 8, 9];
+
+            source.UpdateRange(0, 6, 5, Operation.Addition);
+
+            Assert.IsTrue(source.SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void UpdateRange_Substraction_WorksCorrectly()
+        {
+            int[] source = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            int[] expected = [1, 2, 3, 4, 5, 6, 2, 3, 4];
+
+            source.UpdateRange(6, 8, 5, Operation.Substraction);
+
+            Assert.IsTrue(source.SequenceEqual(expected));
+        }
+    }
+}
